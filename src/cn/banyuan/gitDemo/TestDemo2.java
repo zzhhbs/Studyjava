72ef4d35b2cf06d6a7779e990241d641c1bee30d
zzhhbs <908924736@qq.com>
2022-07-21 10:59:52 +0800

    提交testDemo2 第一次

--- src/cn/banyuan/gitDemo/TestDemo2.java
+++ src/cn/banyuan/gitDemo/TestDemo2.java
@@ -1,8 +1,8 @@
 package cn.banyuan.gitDemo;
 
-public class TestDemo1 {
+public class TestDemo2 {
     public static void main(String[] args) {
-        System.out.println("11111");
-
+        System.out.println("testDemo22222");
     }
+
 }
